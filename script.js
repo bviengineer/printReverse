fd8c817513c06d0af7df72fed405ac30ecd19a47
BVIDev <16726593+bviengineer@users.noreply.github.com>
2017-11-30 04:47:00 -0500

    function that accepts an array as an argument, reverses the array and then prints the reversed array

---
+++ script.js
@@ -0,0 +1,11 @@
+//A function that accepts an array as an argument and prints the contents of the array in reverse order, one item at a time, without modifying the existing array.
+
+var arrayOfColors = ["red", "blue", "pink", "brown", "black", "green", "yellow", "orange"];
+
+function reverseArray(arrayOfColors){
+    var reversedArr = arrayOfColors;
+    reversedArr.reverse();
+    for(var i = 0; i < reversedArr.length; i++){
+        console.log(reversedArr[i]);
+    }    
+}
