bcba6cd2b89c310229999225787a06c9ee82e032
BVIDev <16726593+bviengineer@users.noreply.github.com>
2017-11-30 16:08:13 -0500

    code refactoring

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 //A function that accepts an array as an argument and prints the contents of the array in reverse order, one item at a time, without modifying the existing array.
 
 var arrayOfColors = ["red", "blue", "pink", "brown", "black", "green", "yellow", "orange"];
-var array = arrayOfColors;
 
-function reverseArray(arr){
-    var reversedArr = arr.reverse();
+function reverseArray(array){
+    var reversedArr = array.reverse();
     for(var i = 0; i < reversedArr.length; i++){
         console.log(reversedArr[i]);
     }    
